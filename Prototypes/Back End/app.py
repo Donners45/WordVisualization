260dffe61d04c96b9ae8391aa154a47101f1d063
Sean <stdonnelly@btinternet.com>
2014-11-26 10:51:44 +0000

    batch update- Added new function to search for individual synsets - changes to word v1.01 - futher stabillity fixes

--- Prototypes/Back End/app.py
+++ Prototypes/Back End/app.py
@@ -3,6 +3,7 @@
 # ############################################################################
 import os
 import nltk
+import pickle
 from nltk.corpus import wordnet as wn
 from flask import Flask, jsonify, abort, make_response
 from flask_cors import *
@@ -30,7 +31,9 @@ def parseNameFromSynset(syn):
 def getSynsetHypernyms(syn):
     fs=[]
     for i in syn.hypernyms():
-        struc={"hypernym":str(i)}
+        struc={"word":parseNameFromSynset(i),
+               "group":str(i.pos()),
+               "offset":i.offset()}
         fs.append(struc)
     return fs
 
@@ -69,12 +72,16 @@ def buildListStructure(inputWord):
 # Methods v1.01
 # #
 
-def getChildren01(synonyms): 
+# ######
+# Builds a child list of entities contained within individual synsets in a given list
+# ######
+def extract_child_nodes(synonyms): 
     fstruc = []
     for i in synonyms:
         struc =  { "word": parseNameFromSynset(i),
                    "definition": str(i.definition()),
                    "group": str(i.pos()),
+                   "offset": i.offset(),
                    "examples": getSynsetExamples(i),
                    "hypernyms": getSynsetHypernyms(i)
                    }
@@ -82,14 +89,25 @@ def getChildren01(synonyms):
         fstruc.append(struc)
     return fstruc
 
-
-
-def buildListStructure01(inputWord):
-    wordSet = wn.synsets(inputWord)
+# ######
+# Returns a json structure for a given list of synsets
+# ######
+def build_json_for_root_node(wordset):
     fullStructure = []    
-    fullStructure.append( {"word": str(inputWord),
-                           "group": str(wordSet[0].pos()),
-                           "children" : getChildren01(wordSet)})
+    fullStructure.append( {"word": parseNameFromSynset(wordset[0]),
+                           "children" : extract_child_nodes(wordset)})
+    return fullStructure
+
+# Returns a single json structure for a given synset
+def build_json_for_portion_node(synset):
+    fullStructure = []
+    fullStructure.append( { "word": parseNameFromSynset(synset),
+                   "definition": str(synset.definition()),
+                   "group": str(synset.pos()),
+                   "offset": synset.offset(),
+                   "examples": getSynsetExamples(synset),
+                   "hypernyms": getSynsetHypernyms(synset)
+                   })
     return fullStructure
 
 # ############################################################################
@@ -102,7 +120,13 @@ def get_WordInfo(word_in):
 
 @app.route('/v1.01/word/<string:word_in>', methods=['GET'])
 def get_WordInfo01(word_in):
-    return jsonify({'words' : buildListStructure01(word_in)})
+    word_set = wn.synsets(word_in)
+    return jsonify({'words' : build_json_for_root_node(word_set)})
+
+@app.route('/v1.01/offset/<string:pos>/<int:offset>', methods=['GET'])
+def get_synset_from_pos_offset(pos,offset):
+    word_set = wn._synset_from_pos_and_offset(pos,offset)
+    return jsonify({'word' : build_json_for_portion_node(word_set)})    
 
 @app.errorhandler(404)
 def not_found(error):
