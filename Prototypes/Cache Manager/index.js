6a93124bdefd586d372581a8634b61eca1552da7
Sean <stdonnelly@btinternet.com>
2014-12-08 21:37:17 +0000

    Added Cache functionality to Desolate Taiga

--- Prototypes/Cache Manager/index.js
+++ Prototypes/Cache Manager/index.js
@@ -1,31 +1,49 @@
 var restify = require('restify');
 var request = require('request');
+var NodeCache = require( "node-cache" );
 
 
+var myCache = new NodeCache( { stdTTL: 100, checkperiod: 120 } );
 
 function getWordResponse(word, fn) {
-console.log('called');
 
-var url = ('http://obscure-river-4096.herokuapp.com/v1.01/word/' + word);
+	var url = ('http://obscure-river-4096.herokuapp.com/v1.01/word/' + word);
 
-console.log(url);
+	console.log(url);
 
-request(url, function(error, response, body){
-		if (!error){
-			fn(body);
-		}else{
-			console.log('fail');
-		}
-	});
-  }
-
-function respond(req, res, next) {  
-
-getWordResponse((req.params.word), function(getWord){
-	res.send(JSON.parse(getWord));
+	request(url, function(error, response, body){
+			if (!error){
+			
+				myCache.set(word, JSON.parse(body), function( err, success ){
+					if( !err && success ){
+					console.log( success );
+						}
+					});
+				fn(body);
+			}else{
+				console.log('fail');
+			}
 		});
-  next();
+	}
+
+
+  function respond(req, res, next) {  
+	var wordValue = req.params.word;
+	var value =  myCache.get(wordValue);
+	
+	if (Object.keys(value).length > 0) {
+		console.log('cache');
+		res.send(value[wordValue]);
+	}else{
+	console.log('live');
+		getWordResponse((req.params.word), function(getWord){
+			res.send(JSON.parse(getWord));
+		});
+	}
+		next();
+
 }
+
 
 var server = restify.createServer();
 server.use(restify.fullResponse());
